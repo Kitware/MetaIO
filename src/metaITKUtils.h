4a73c9791aea372422d96d827081f6e2ad63679e
Brad King <brad.king@kitware.com>
2010-06-01 10:42:36 -0400

    STYLE: Simplify MetaIO copyright notices
    Remove references to Subversion keywords.  This makes the text
    consistent in all files.  Also replace reference to non-existing file
    ITKCopyright.txt with a Copyright.txt.
    
    SVN-Revision: 2272

--- src/metaITKUtils.h
+++ src/metaITKUtils.h
@@ -1,19 +1,14 @@
-/*=========================================================================
+/*============================================================================
+  MetaIO
+  Copyright 2000-2010 Insight Software Consortium
 
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: metaITKUtils.h,v $
-  Language:  C++
-  Date:      $Date: 2007/05/31 13:53:13 $
-  Version:   $Revision: 1.9 $
+  Distributed under the OSI-approved BSD License (the "License");
+  see accompanying file Copyright.txt for details.
 
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+============================================================================*/
 // Utility file - definition of loadImage
 // Templated over the Pixel Type
 
