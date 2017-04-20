1328ee7d93fadc91a30477cfda5c38f47cc47983
Ben Boeckel <ben.boeckel@kitware.com>
2017-04-20 15:41:14 -0400

    metaBlob: fix a typo in a comment

--- src/metaBlob.cxx
+++ src/metaBlob.cxx
@@ -416,7 +416,7 @@ M_Read(void)
       char c = ' ';
       while( (c!='\n') && (!m_ReadStream->eof()))
         {
-        c = static_cast<char>(m_ReadStream->get());// to avoid unrecognize charactere
+        c = static_cast<char>(m_ReadStream->get());// to avoid unrecognized characters
         }
       }
   }
