817c01585c8877d6b9e2e296516feaa7fb91019a
Dženan Zukić <dzenan.zukic@kitware.com>
2019-05-07 17:17:18 -0400

    STYLE: remove pre-C++11 code from metaTypes.h

--- src/metaTypes.h
+++ src/metaTypes.h
@@ -35,17 +35,6 @@
 namespace METAIO_NAMESPACE {
 #endif
 
-#if defined(_MSC_VER) && (_MSC_VER <= 1500) // until Visual Studio 2008
-typedef signed __int8       int8_t;
-typedef signed __int16      int16_t;
-typedef signed __int32      int32_t;
-typedef unsigned __int8     uint8_t;
-typedef unsigned __int16    uint16_t;
-typedef unsigned __int32    uint32_t;
-typedef signed __int64      int64_t;
-typedef unsigned __int64    uint64_t;
-#endif
-
 typedef char                MET_ASCII_CHAR_TYPE;
 typedef int8_t              MET_CHAR_TYPE;
 typedef uint8_t             MET_UCHAR_TYPE;
@@ -55,13 +44,8 @@ typedef int32_t             MET_INT_TYPE;
 typedef uint32_t            MET_UINT_TYPE;
 typedef int32_t             MET_LONG_TYPE;
 typedef uint32_t            MET_ULONG_TYPE;
-#if defined(_WIN32) && !defined(__CYGWIN__) && !defined(__MING_W32__)
-typedef __int64             MET_LONG_LONG_TYPE;
-typedef unsigned __int64    MET_ULONG_LONG_TYPE;
-#else
 typedef int64_t             MET_LONG_LONG_TYPE;
 typedef uint64_t            MET_ULONG_LONG_TYPE;
-#endif
 typedef float               MET_FLOAT_TYPE;
 typedef double              MET_DOUBLE_TYPE;
 typedef char *              MET_STRING_TYPE;
@@ -189,8 +173,7 @@ const char MET_InterpolationTypeName[MET_NUM_INTERPOLATION_TYPES][17] = {
 
 
 #define MET_MAX_NUMBER_OF_FIELD_VALUES 4096
-//
-//
+
 // Structure used to define a field
 // (variable = value definition) in a MetaFile
 typedef struct
