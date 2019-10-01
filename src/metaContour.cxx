702bbe0d6692c25e99c3d37a1fdc7d5973072adf
Sean McBride <sean@rogue-research.com>
2019-10-01 19:53:18 -0400

    Fixed -Wfloat-conversion warnings

--- src/metaContour.cxx
+++ src/metaContour.cxx
@@ -365,7 +365,7 @@ M_Read()
   mF = MET_GetFieldRecord("Closed", &m_Fields);
   if(mF->defined)
     {
-    if(mF->value[0])
+    if(mF->value[0] != 0.0)
       {
       m_Closed = true;
       }
@@ -374,7 +374,7 @@ M_Read()
   mF = MET_GetFieldRecord("DisplayOrientation", &m_Fields);
   if(mF->defined)
     {
-    if(mF->value[0])
+    if(mF->value[0] != 0.0)
       {
       m_DisplayOrientation = (int)mF->value[0];
       }
@@ -383,7 +383,7 @@ M_Read()
   mF = MET_GetFieldRecord("PinToSlice", &m_Fields);
   if(mF->defined)
     {
-    if(mF->value[0])
+    if(mF->value[0] != 0.0)
       {
       m_AttachedToSlice = (long int)mF->value[0];
       }
